a4d0877c6d74a515642c38bf23b1d497ba0034de
Lauren Powell-Smothers <lauren.powells@gmail.com>
2023-06-19 16:49:32 -0400

    Update animal-sounds.js

--- animal-sounds.js
+++ animal-sounds.js
@@ -1,6 +1,27 @@
 // Write code here
+function cowSounds() {
+    console.log("Cows go MOOO!")
+}
 
+function dogSounds() {
+    console.log("Dogs go WOOOF!")
+}
 
+function duckSounds() {
+    console.log("Ducks go QUACK!")
+}
+
+function whatSound(animals) {
+    if (animals == "Cow"){
+        cowSounds();
+    } else if (animals == "Dog"){
+        dogSounds();
+    } else if (animals == "Duck"){
+        duckSounds();
+    } else {
+        console.log ("I'm not sure what noise that animal makes.");
+    }
+}
 
 // DONT CHANGE. THIS IS FOR TESTING
 let animals = [ "Bat", "Cat", "Cat", "Dog", "Cow", "Duck", "Dog", "Fish", "Cow", "Dog", "Duck", "Duck" ]
